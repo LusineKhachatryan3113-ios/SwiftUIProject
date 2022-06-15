ba429bff529eb02775ef70c75280cd6a4a4bc006
LusineKhachatryan3113-ios <101631371+LusineKhachatryan3113-ios@users.noreply.github.com>
2022-06-15 12:08:50 +0400

    Add files via upload

---
+++ Authentication/Authentication/FireBaseApp.swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticFireBaseApp.swift
+//  Authentication
+//
+//  Created by Lusine on 5/6/22.
+//
+
+import SwiftUI
+
+//@main
+
+struct FireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
+
+
